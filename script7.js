37b600dc5f1adcab31d5addcb7d5ae4f4dfd4230
Myroslav Shulha <myrosllavsh@gmail.com>
2025-01-29 18:59:14 +0200

    fjdk

--- script7.js
+++ script7.js
@@ -28,27 +28,152 @@
 
 
 
-class Discount{
-    apply(price){
-        return price;
-    }
-}
+// class Discount{
+//     apply(price){
+//         return price;
+//     }
+// }
 
-class MirosAnt extends Discount{
-    apply(price){
-        return price * 2;
-    }
-}
+// class MirosAnt extends Discount{
+//     apply(price){
+//         return price * 2;
+//     }
+// }
 
-class MrAnt extends Discount{
-    apply(price){
-        return price * 4;
-    }
-}
+// class MrAnt extends Discount{
+//     apply(price){
+//         return price * 4;
+//     }
+// }
 
 
-class NewRes extends Discount{
-    apply(price){
-        console.log(price * 7)
-    }
-}
+// class NewRes extends Discount{
+//     apply(price){
+//         console.log(price * 7)
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class SQLDatabase{
+//     connect(){
+//         console.log("Connecting to SQL Database...");
+//     }
+// }
+
+// class Application{
+//     constructor(){
+//         this.database = new SQLDatabase();
+//     }
+    
+//     run(){
+//         this.database.connect();
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+// class Database{
+//     connect(){}
+// }
+
+// class SQLDatabase extends Database {
+//     connect(){
+//         console.log('Connecting to SQL Database...')
+//     }
+// }
+
+// class Application{
+//     constructor(database){
+//         this.database = database;
+//     }
+    
+//     run(){
+//         this.database.connect();
+//     }
+// }
+
+// const db = new SQLDatabase();
+// const app = new Application(db);
+// app.run;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //Є базовий клас в якому оголошений пустий метод Створити клас клас наслідник в якому 2 приватних поля які будуть виводитися в переопридільоному методі Створити клас наслідник в якому переопридільоний метод буде виводити повідомлення Hello world
+// class Sonic{
+//     print(){}
+// }
+
+// class Miros extends Sonic{
+//     #name = "Myroslav Shulha"
+//     #age = 14
+//     print(){
+//         console.log(this.#name, this.#age)
+//     }
+// }
+
+// class Welcome extends Sonic{
+//     print(){
+//         console.log('Hello World')
+//     }
+// }
+
+
+// let a = new Miros()
+// a.print()
+// let b = new Welcome()
+// b.print()
+
+
+
+
+
+
+
+
+
+
+
+
